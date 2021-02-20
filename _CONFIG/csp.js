1ec00dd9c7df0d8730d0313e06493012c5a323d5
Dan Moneypenny <catullus1973@gmail.com>
2021-02-20 10:23:47 -0800

    adjust csp

--- _CONFIG/csp.js
+++ _CONFIG/csp.js
@@ -2,9 +2,20 @@ const policies = {
   directives: {
     "default-src": ["'self'"],
     "img-src": ["'self'", "*.cloudinary.com", "data:"],
-    "font-src": ["'self'", "https://fonts.googleapis.com", "https://fonts.gstatic.com"],
-    "style-src-elem": ["'self'", "'nonce-htueffffhehendgvhfdhggfwasaweqfkjkjhvdhghsfdfkjhpofjfbndfhsflkdlknfkjbnlfvyucdrsdysf'", "https://fonts.googleapis.com", "https://fonts.gstatic.com"],
-    "style-src": ["'self'", "'nonce-htueffffhehendgvhfdhggfwasaweqfkjkjhvdhghsfdfkjhpofjfbndfhsflkdlknfkjbnlfvyucdrsdysf'"]
+    "font-src": [
+      "'self'", 
+      "https://fonts.googleapis.com", 
+      "https://fonts.gstatic.com"
+    ],
+    "style-src-elem": [
+      "'self'", 
+      "'nonce-htueffffhehendgvhfdhggfwasaweqfkjkjhvdhghsfdfkjhpofjfbndfhsflkdlknfkjbnlfvyucdrsdysf'", 
+      "https://fonts.googleapis.com", 
+      "https://fonts.gstatic.com"
+    ],
+    "style-src": [
+      "'self'", 
+      "'nonce-htueffffhehendgvhfdhggfwasaweqfkjkjhvdhghsfdfkjhpofjfbndfhsflkdlknfkjbnlfvyucdrsdysf'"]
   }
 }
 
