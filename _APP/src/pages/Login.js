f543541507a2a34a080f0a0ae94cefbf68534c83
Dan Moneypenny <catullus1973@gmail.com>
2021-02-24 01:37:54 -0800

    add login form

--- _APP/src/pages/Login.js
+++ _APP/src/pages/Login.js
@@ -1,11 +1,44 @@
 import React from 'react';
 
 const Login = () => {  
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('email: ', email);
+    console.log('password: ', password);
+    setEmail('');
+    setPassword('')
+  }
 
   return ( 
-    <React.Fragment>
-      <h1>Login Page</h1>
-    </React.Fragment>
+
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>log in to application</legend>
+        <label htmlFor="email">email</label>
+        <input 
+          type="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="email..."
+          autoComplete="off"
+        />
+        <label htmlFor="password">password</label>
+        <input 
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}          
+          placeholder="password..."
+          autoComplete="off"
+        /> 
+        <hr/>
+        <button type="submit">submit credentials</button>       
+      </fieldset>
+    </form>
     
    );
 }
