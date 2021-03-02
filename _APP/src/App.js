f52443f002641c8aea6b66e5c15c0f15786d313a
Dan Moneypenny <catullus1973@gmail.com>
2021-03-01 22:36:37 -0800

    add lazy loading

--- _APP/src/App.js
+++ _APP/src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { AuthContext, AuthProvider } from './contexts/authentication';
 import Layout from './layout';
 import Home from './pages/Home';
 import Login from './pages/Login';
-import MainDashboard from './pages/MainDashboard';
+//import MainDashboard from './pages/MainDashboard';
 import NotFound from './pages/NotFound';
-import Projects from './pages/Projects';
-import Signup from './pages/Signup';
+//import Projects from './pages/Projects';
+//import Signup from './pages/Signup';
+
+const MainDashboard = lazy(() => import('./pages/MainDashboard'));
+const Projects = lazy(() => import('./pages/Projects'));
+const Signup = lazy(() => import('./pages/Signup'));
 
 const PrivateRoute = ({ children, ...rest }) => {
   const auth = React.useContext(AuthContext);
@@ -27,6 +31,7 @@ const PrivateRoute = ({ children, ...rest }) => {
 
 const AppRoutes = () => {
   return (
+    <Suspense fallback={<div>loading...</div>}>
     <Switch>
       <Route exact path='/'>
         <Home/>
@@ -47,6 +52,7 @@ const AppRoutes = () => {
         <NotFound/>
       </Route>                   
     </Switch>
+    </Suspense>
   )
 }
 
