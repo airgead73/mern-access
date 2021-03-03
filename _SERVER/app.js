cfb59dba703971e6470fcd281640d0ac87ab16f4
Dan Moneypenny <catullus1973@gmail.com>
2021-03-03 00:35:41 -0800

    add session middleware

--- _SERVER/app.js
+++ _SERVER/app.js
@@ -33,15 +33,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, './../_PUBLIC')));
 app.use(cookieParser());
-// app.use(session({
-//   secret: SESSION_SECRET,
-//   saveUninitialized: true,
-//   resave: true,
-//   cookie: {
-//     httpOnly: true,
-//     maxAge: parseInt(SESSION_AGE)
-//   }
-// }));
+app.use(session({
+  secret: SESSION_SECRET,
+  saveUninitialized: true,
+  resave: true,
+  cookie: {
+    httpOnly: true,
+    maxAge: parseInt(SESSION_AGE)
+  }
+}));
 
 app.use((req, res, next) => {
 
