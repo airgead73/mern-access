b21a4fc42c1efa437dfa1680398caf59cc06d371
Dan Moneypenny <catullus1973@gmail.com>
2021-03-14 00:58:10 -0800

    update header

--- _APP/src/layout/Header.js
+++ _APP/src/layout/Header.js
@@ -4,15 +4,15 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 const Header = (props) => {
 
-  const { authState, logout, isAuthenticated } = React.useContext(AuthContext);
-  const { loginWithRedirect } = useAuth0;
+  const { loginWithRedirect, logout } = useAuth0();
 
   return ( 
     <header>
       <h1>project manager</h1>
-      <p>{authState.userInfo.firstName}</p>
+      
       <button onClick={loginWithRedirect}>login</button>
-      {isAuthenticated() && (<button onClick={() => logout()}>logout</button>)}
+      <button onClick={() => logout({ returnTo: window.location.origin })}>logout</button>
+      
       
     </header>
    );
