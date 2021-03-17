cf48452f4e3277209d47c62151bc5d08c2195afd
Dan Moneypenny <catullus1973@gmail.com>
2021-03-16 18:22:41 -0700

    remove user and authentication from api

--- _CONFIG/constants.js
+++ _CONFIG/constants.js
@@ -3,8 +3,5 @@ module.exports = {
   PORT_CLIENT: process.env.PORT_CLIENT,
   DB_DEV: process.env.DB_DEV,
   DB: process.env.DB,
-  ISDEV: process.env.NODE_ENV === 'development',
-  JWT_SECRET: process.env.JWT_SECRET,
-  SESSION_SECRET: process.env.SESSION_SECRET,
-  SESSION_AGE: 3600000
+  ISDEV: process.env.NODE_ENV === 'development'
 }
--- _SERVER/app.js
+++ _SERVER/app.js
@@ -4,10 +4,7 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 const helmet = require('helmet');
 const handleError = require('./src/middleware/handleError')
-const session = require('express-session');
 const policies = require('../_CONFIG/csp');
-const { SESSION_SECRET, SESSION_AGE, JWT_SECRET } = require('../_CONFIG/constants');
-const jwt = require('express-jwt');
 
 /**
  * INTIALIZE APP
@@ -33,20 +30,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, './../_PUBLIC')));
 app.use(cookieParser());
-// app.use(session({
-//   secret: SESSION_SECRET,
-//   saveUninitialized: true,
-//   resave: true,
-//   cookie: {
-//     httpOnly: true,
-//     maxAge: parseInt(SESSION_AGE)
-//   }
-// }));
 
 app.use((req, res, next) => {
 
-  console.log(req.headers);
-  next()
+  console.log('headers:', req.headers);
+  next();
 
 });
 
